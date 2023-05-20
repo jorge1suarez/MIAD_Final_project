2f270208a2a7f25baff91039c0876196eec7007a
Freddy <frmendozat@uni.pe>
2023-05-20 09:36:23 -0500

    upd

--- job/run.sh
+++ job/run.sh
@@ -1,11 +1,11 @@
-cd ~/MIAD_Final_project/etl/
+cd /home/freddy_12_120/MIAD_Final_project/etl/
 conda activate etl
 python3 etl_first.py
 
-cd ~/MIAD_Final_project/training/
+cd /home/freddy_12_120/MIAD_Final_project/training/
 conda activate api
 python3 validation.py
 python3 train_cpu.py
 
-cd ~/MIAD_Final_project/predictions/
+cd /home/freddy_12_120/MIAD_Final_project/predictions/
 python3 predictions.py
